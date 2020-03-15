fdbeb4f0faf79d1ddac31ac9df501eb96fd6178a
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2020-03-15 15:28:24 +0200

    sanitize video list

--- src/pages/videos/index.tsx
+++ src/pages/videos/index.tsx
@@ -1,14 +1,36 @@
 import * as React from 'react';
 
+import { CardDeck, Card } from 'react-bootstrap';
+
+const mockVids = [
+  {
+    title: 'sample video',
+    filename: 'any',
+  },
+  {
+    title: 'another video',
+    filename: 'differentFile',
+  }
+];
+
 export default class Videos extends React.Component {
 
   public render() {
     return <div>
-      <h1>list of videos</h1>
-      <ul>
-        <li>none</li>
-        <li>yetanother</li>
-      </ul>
+      <h1>List of videos</h1>
+      <CardDeck>
+        {mockVids.map(({ title, filename }) =>
+          <Card>
+            <Card.Body>
+              <Card.Title>title</Card.Title>
+              <Card.Text>{title}</Card.Text>
+              <Card.Title>filename</Card.Title>
+              <Card.Text>{filename}</Card.Text>
+            </Card.Body>
+          </Card>
+        )}
+
+      </CardDeck>
     </div>;
   }
 
