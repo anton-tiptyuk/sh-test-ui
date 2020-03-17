88d6d225c15b15c05b1eec251af80068ada372f1
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2020-03-17 12:36:46 +0200

    going towards final-form -- gets tooo complex

--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 
-import { Form, Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
+// import { Form, Field } from 'react-final-form'
+import * as FForm from 'react-final-form'
 
 import api from '../../common/api';
 import { gqlApi } from '../../common/gql';
@@ -73,13 +75,17 @@ class Upload extends React.Component<IActions, IUploadState> {
     return status;
   }
 
-  onFormSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  // onFormSubmit = (e: React.FormEvent) => {
+  //   e.preventDefault();
 
-    this.setState(
-      { submitInProgress: true },
-      () => this.submitFile().then(status => this.resetForm({ ...defaultState, status }))
-    );
+  //   this.setState(
+  //     { submitInProgress: true },
+  //     () => this.submitFile().then(status => this.resetForm({ ...defaultState, status }))
+  //   );
+  // }
+
+  onFormSubmit = (data: any) => {
+    console.log(data);
   }
 
   resetForm = (resetState: IUploadState = defaultState) => {
@@ -90,34 +96,62 @@ class Upload extends React.Component<IActions, IUploadState> {
   public render() {
     const { file, submitInProgress, status } = this.state;
 
-    return <Form onSubmit={this.onFormSubmit} >
-      <h1>File upload</h1>
+    return <FForm.Form {...{
+      onSubmit: this.onFormSubmit,
+      render: ({ handleSubmit, form }) =>
+        <Form
+          onSubmit={
+            async (event: any) => {
+              console.log(event);
+              await handleSubmit(event);
+              // form.reset();
+            }}
+        >
+          <h1>File upload</h1>
 
-      <Form.Group controlId='title'>
-        <Form.Label>Title</Form.Label>
-        <Form.Control type='text' placeholder='title' value={this.state.title} onChange={(e: any) => { this.setState({ title: e.target.value }) }} />
-      </Form.Group>
+          <Form.Group controlId='title'>
+            <Form.Label>Title</Form.Label>
+            <FForm.Field
+              name='title'
+              type='text'
+              placeholder='title'
+              component={Form.Control as any}
+            />
+          </Form.Group>
 
-      <Form.Group controlId='filename'>
-        <Form.Label>Filename</Form.Label>
-        <Form.Control type='text' placeholder='filename' value={this.state.filename} onChange={(e: any) => { this.setState({ filename: e.target.value }) }} />
-      </Form.Group>
 
-      <Form.Group controlId='description'>
-        <Form.Label>Description</Form.Label>
-        <Form.Control type='text' placeholder='description' value={this.state.description} onChange={(e: any) => { this.setState({ description: e.target.value }) }} />
-      </Form.Group>
-
-      <Form.Group controlId='file'>
-        <Form.Control type='file' accept='video/*' ref={this.fileInputRef} onChange={(e: any) => this.onFileSelected(e.target.files)} />
-      </Form.Group>
-
-      <Button variant='primary' type='submit' disabled={!file || submitInProgress}>Submit</Button>
-        &nbsp;
-        <Button onClick={() => this.resetForm()}>Reset</Button>
-      <p>{status}</p>
-    </Form>
+          <Button variant='primary' type='submit'>Submit</Button>
+          &nbsp;
+          <Button onClick={form.reset}>Reset</Button>
+          <p>{status}</p>
+        </Form>,
+    }} />;
   }
+
+
+  // <Form.Group controlId='title'>
+  // <Form.Label>Title</Form.Label>
+  // <Form.Control type='text' placeholder='title' value={this.state.title} onChange={(e: any) => { this.setState({ title: e.target.value }) }} />
+  // </Form.Group>
+
+  // <Form.Group controlId='filename'>
+  // <Form.Label>Filename</Form.Label>
+  // <Form.Control type='text' placeholder='filename' value={this.state.filename} onChange={(e: any) => { this.setState({ filename: e.target.value }) }} />
+  // </Form.Group>
+
+  // <Form.Group controlId='description'>
+  // <Form.Label>Description</Form.Label>
+  // <Form.Control type='text' placeholder='description' value={this.state.description} onChange={(e: any) => { this.setState({ description: e.target.value }) }} />
+  // </Form.Group>
+
+  // <Form.Group controlId='file'>
+  // <Form.Control type='file' accept='video/*' ref={this.fileInputRef} onChange={(e: any) => this.onFileSelected(e.target.files)} />
+  // </Form.Group>
+
+  // <Button variant='primary' type='submit' disabled={!file || submitInProgress}>Submit</Button>
+  // &nbsp;
+  // <Button onClick={() => this.resetForm()}>Reset</Button>
+
 
 }
 
