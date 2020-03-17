cea7774c5739cd291497a0f017cdd93c14e70a29
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2020-03-17 12:41:50 +0200

    test cut off

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './pages';
-
-test('renders upload a file text', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/upload a file/i);
-  expect(linkElement).toBeInTheDocument();
-});
